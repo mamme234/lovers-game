aab0b8161d7d2bc55130050e0932e0015a91cd62
mamme234 <ghazimuhammadilyas@gmail.com>
2026-07-24 23:03:49 +0300

    Update dashboard.js

--- frontend/js/pages/dashboard.js
+++ frontend/js/pages/dashboard.js
@@ -1,6 +1,6 @@
 import { AnimationManager } from '../animations.js';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://lovers-game.onrender.com/api';
 const animationManager = new AnimationManager();
 
 export async function loadDashboard(user, couple, token) {
