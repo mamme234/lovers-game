12c8b7a7ab7a03741b875bae33fdb3dffc0ee23c
mamme234 <ghazimuhammadilyas@gmail.com>
2026-07-24 23:04:48 +0300

    Update games.js

--- frontend/js/pages/games.js
+++ frontend/js/pages/games.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://lovers-game.onrender.com/api';
 
 export async function loadGames(user, couple, token, socket) {
   if (!couple) {
