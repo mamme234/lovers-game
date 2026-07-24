1f3dc3a0caca075beb2749dbece04e359fda29fd
mamme234 <ghazimuhammadilyas@gmail.com>
2026-07-24 21:25:33 +0300

    Update auth.js

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,6 +1,6 @@
 import { getTelegramUser, getTelegramInitData, showTelegramAlert } from './telegram.js';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://lovers-game.onrender.com/api';
 
 export async function initializeAuth(app) {
   try {
